478719a29ef73237e2e2c6899d9a9e1caab11313
Alexandr Borovets <borovets9494@mail.ru>
2020-03-01 23:26:16 +0300

    Add Command interface

---
+++ src/main/java/by/training/parser/command/Command.java
@@ -0,0 +1,8 @@
+package by.training.parser.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Command {
+	String execute(HttpServletRequest request, HttpServletResponse response);
+}
