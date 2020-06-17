89198093bba4d2fba02a97406e817b772f629c38
Alexandr Borovets <borovets9494@mail.ru>
2020-06-17 16:44:43 +0300

    Create Override annotation to doGet, doPost methods in Controller

--- src/main/java/by/training/parser/controller/Controller.java
+++ src/main/java/by/training/parser/controller/Controller.java
@@ -1,6 +1,8 @@
 package by.training.parser.controller;
 
-import java.io.IOException;
+import by.training.parser.command.Command;
+import by.training.parser.command.PageEnum;
+import by.training.parser.factory.CommandFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -8,32 +10,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import by.training.parser.command.Command;
-import by.training.parser.command.PageEnum;
-import by.training.parser.factory.CommandFactory;
+import java.io.IOException;
 
 @SuppressWarnings("serial")
 @WebServlet("/controller")
 @MultipartConfig(maxFileSize = 10240)
 public class Controller extends HttpServlet {
 
-	private static final CommandFactory COMMAND_FACTORY = new CommandFactory();
+    private static final CommandFactory COMMAND_FACTORY = new CommandFactory();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		processRequest(request, response);
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		processRequest(request, response);
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
-	private void processRequest(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		Command command = COMMAND_FACTORY.defineCommand(request);
-		String path = command.execute(request, response);
-		request.getRequestDispatcher(path != null ? path : PageEnum.HOME_PAGE.getValue()).forward(request, response);
-	}
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Command command = COMMAND_FACTORY.defineCommand(request);
+        String path = command.execute(request, response);
+        request.getRequestDispatcher(path != null ? path : PageEnum.HOME_PAGE.getValue()).forward(request, response);
+    }
 }
