788a19d31f58697c03e559618fb8f03a6868f7e6
Alexandr Borovets <borovets9494@mail.ru>
2020-06-17 18:35:51 +0300

    Decompose large methods to small methods in StAXGemBuilder class

--- src/main/java/by/training/parser/builder/StAXGemBuilder.java
+++ src/main/java/by/training/parser/builder/StAXGemBuilder.java
@@ -18,118 +18,121 @@ import static javax.xml.bind.DatatypeConverter.parseDate;
 
 public class StAXGemBuilder extends GemBuilder {
 
-    private static final Logger LOGGER = LogManager.getLogger(StAXGemBuilder.class);
-    private static final String EMPTY_STRING = "";
-    private XMLInputFactory inputFactory;
+	private static final Logger LOGGER = LogManager.getLogger(StAXGemBuilder.class);
+	private static final String EMPTY_STRING = "";
+	private XMLInputFactory inputFactory;
 
-    public StAXGemBuilder() {
-        inputFactory = XMLInputFactory.newInstance();
-        inputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
-    }
+	public StAXGemBuilder() {
+		inputFactory = XMLInputFactory.newInstance();
+		inputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+	}
 
-    public void buildGemList(InputStream sourceFile) throws XMLParserException {
-        gemList.clear();
-        String name;
-        try {
-            XMLStreamReader reader = inputFactory.createXMLStreamReader(sourceFile);
-            while (reader.hasNext()) {
-                int type = reader.next();
-                if (type == XMLStreamConstants.START_ELEMENT) {
-                    name = reader.getLocalName();
-                    if (GemEnum.valueOf(name.toUpperCase()) == GemEnum.GEM) {
-                        gemList.add(buildGem(reader));
-                    }
-                }
-            }
-        } catch (XMLStreamException e) {
-            LOGGER.warn("Parsing failure", e);
-            throw new XMLParserException("Parsing failure", e);
-        } finally {
-            closeInputStream(sourceFile);
-        }
-    }
+	public void buildGemList(InputStream sourceFile) throws XMLParserException {
+		gemList.clear();
+		String name;
+		try {
+			XMLStreamReader reader = inputFactory.createXMLStreamReader(sourceFile);
+			while (reader.hasNext()) {
+				int type = reader.next();
+				if (type == XMLStreamConstants.START_ELEMENT) {
+					name = reader.getLocalName();
+					if (GemEnum.valueOf(name.toUpperCase()) == GemEnum.GEM) {
+						gemList.add(buildGem(reader));
+					}
+				}
+			}
+		} catch (XMLStreamException e) {
+			LOGGER.warn("Parsing failure", e);
+			throw new XMLParserException("Parsing failure", e);
+		} finally {
+			closeInputStream(sourceFile);
+		}
+	}
 
-    private Gem buildGem(XMLStreamReader reader) throws XMLStreamException {
-        Gem gem = new Gem();
-        gem.setId(reader.getAttributeValue(null, GemEnum.ID.getValue()));
-        String name;
-        int type;
-        while (reader.hasNext()) {
-            type = reader.next();
-            switch (type) {
-                case XMLStreamConstants.START_ELEMENT:
-                    name = reader.getLocalName();
-                    switch (GemEnum.valueOf(name.toUpperCase())) {
-                        case NAME:
-                            gem.setName(getXMLText(reader));
-                            break;
-                        case PRECIOUSNESS:
-                            gem.setPreciousnessType(PreciousnessType.valueOf(getXMLText(reader).toUpperCase()));
-                            break;
-                        case ORIGIN:
-                            gem.setOrigin(getXMLText(reader));
-                            break;
-                        case VISUALPARAMETERS:
-                            setVisualParameter(reader, gem.getVisualParameter());
-                            break;
-                        case VALUE:
-                            gem.setValue(Double.valueOf(getXMLText(reader)));
-                            break;
-                        case PRODUCTIONDATE:
-                            gem.setProductionDate(parseDate(getXMLText(reader)).getTime());
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case XMLStreamConstants.END_ELEMENT:
-                    name = reader.getLocalName();
-                    if (GemEnum.GEM == GemEnum.valueOf(name.toUpperCase())) {
-                        return gem;
-                    }
-                    break;
-                default:
-                    throw new XMLStreamException("Unknown element in tag gem");
-            }
-        }
-        throw new XMLStreamException("Unknown element in tag gem");
-    }
+	private Gem buildGem(XMLStreamReader reader) throws XMLStreamException {
+		Gem gem = new Gem();
+		gem.setId(reader.getAttributeValue(null, GemEnum.ID.getValue()));
+		GemEnum currentValue = null;
+		int type;
+		while (reader.hasNext()) {
+			type = reader.next();
+			if (type == XMLStreamConstants.START_ELEMENT) {
+				currentValue = GemEnum.valueOf(reader.getLocalName().toUpperCase());
+				handleGemCurrentValue(currentValue, reader, gem);
+			}
+			if (type == XMLStreamConstants.END_ELEMENT) {
+				currentValue = GemEnum.valueOf(reader.getLocalName().toUpperCase());
+				if (GemEnum.GEM == currentValue) {
+					return gem;
+				}
+			}
+		}
+		throw new XMLStreamException("Unknown element in tag gem");
+	}
 
-    private void setVisualParameter(XMLStreamReader reader, VisualParameter visualParameter) throws XMLStreamException {
-        int type;
-        String name;
-        while (reader.hasNext()) {
-            type = reader.next();
-            switch (type) {
-                case XMLStreamConstants.START_ELEMENT:
-                    name = reader.getLocalName();
-                    switch (GemEnum.valueOf(name.toUpperCase())) {
-                        case COLOR:
-                            visualParameter.setGemColor(GemColor.valueOf(getXMLText(reader).toUpperCase()));
-                            break;
-                        case TRANSPARENCY:
-                            visualParameter.setTransparencyValue(Integer.valueOf(getXMLText(reader)));
-                            break;
-                        case FACETS:
-                            visualParameter.setFacetCount(Integer.valueOf(getXMLText(reader)));
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case XMLStreamConstants.END_ELEMENT:
-                    name = reader.getLocalName();
-                    if (GemEnum.valueOf(name.toUpperCase()) == GemEnum.VISUALPARAMETERS) {
-                        return;
-                    }
-                    break;
-                default:
-                    throw new XMLStreamException("Unknown element in tag visualParameters");
-            }
-        }
-    }
+	private void handleGemCurrentValue(GemEnum currentValue, XMLStreamReader reader, Gem gem)
+			throws XMLStreamException {
+		switch (currentValue) {
+		case NAME:
+			gem.setName(getXMLText(reader));
+			break;
+		case PRECIOUSNESS:
+			gem.setPreciousnessType(PreciousnessType.valueOf(getXMLText(reader).toUpperCase()));
+			break;
+		case ORIGIN:
+			gem.setOrigin(getXMLText(reader));
+			break;
+		case VISUALPARAMETERS:
+			setVisualParameter(reader, gem.getVisualParameter());
+			break;
+		case VALUE:
+			gem.setValue(Double.valueOf(getXMLText(reader)));
+			break;
+		case PRODUCTIONDATE:
+			gem.setProductionDate(parseDate(getXMLText(reader)).getTime());
+			break;
+		default:
+			break;
+		}
+	}
 
-    private String getXMLText(XMLStreamReader reader) throws XMLStreamException {
-        return XMLStreamConstants.END_ELEMENT == reader.next() ? EMPTY_STRING : reader.getText();
-    }
+	private void setVisualParameter(XMLStreamReader reader, VisualParameter visualParameter) throws XMLStreamException {
+		int type;
+		GemEnum currentValue = null;
+		while (reader.hasNext()) {
+			type = reader.next();
+			if (type == XMLStreamConstants.START_ELEMENT) {
+				currentValue = GemEnum.valueOf(reader.getLocalName().toUpperCase());
+				handleVisualParameterCurrentValue(currentValue, reader, visualParameter);
+			}
+			if (type == XMLStreamConstants.END_ELEMENT) {
+				currentValue = GemEnum.valueOf(reader.getLocalName().toUpperCase());
+				if (currentValue == GemEnum.VISUALPARAMETERS) {
+					return;
+				}
+			}
+		}
+		throw new XMLStreamException("Unknown element in tag visualParameters");
+	}
+
+	private void handleVisualParameterCurrentValue(GemEnum currentValue, XMLStreamReader reader,
+			VisualParameter visualParameter) throws XMLStreamException {
+		switch (currentValue) {
+		case COLOR:
+			visualParameter.setGemColor(GemColor.valueOf(getXMLText(reader).toUpperCase()));
+			break;
+		case TRANSPARENCY:
+			visualParameter.setTransparencyValue(Integer.valueOf(getXMLText(reader)));
+			break;
+		case FACETS:
+			visualParameter.setFacetCount(Integer.valueOf(getXMLText(reader)));
+			break;
+		default:
+			break;
+		}
+	}
+
+	private String getXMLText(XMLStreamReader reader) throws XMLStreamException {
+		return XMLStreamConstants.END_ELEMENT == reader.next() ? EMPTY_STRING : reader.getText();
+	}
 }
